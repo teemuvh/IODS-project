1119dfd7c734b4b6bd86f2e186b48610b3538ccf
Teemu Vahtola <teemu.vahtola@helsinki.fi>
2022-11-23 14:45:26 +0200

    Updated create_alc.R

--- data/create_alc.R
+++ data/create_alc.R
@@ -3,6 +3,9 @@
 # Data (student-mat.csv and student-por.csv) obtained from: 
 # UCI Machine Learning Repository, Student Performance Data (incl. Alcohol consumption) 
 # Url to original data: https://archive.ics.uci.edu/ml/datasets/Student+Performance
+# Reference: P. Cortez and A. Silva. Using Data Mining to Predict Secondary School Student Performance. 
+# In A. Brito and J. Teixeira Eds., Proceedings of 5th FUture BUsiness TEChnology Conference (FUBUTEC 2008) pp. 5-12,
+# Porto, Portugal, April, 2008, EUROSIS, ISBN 978-9077381-39-7.
 
 # Import dplyr and readr
 library(dplyr)
